b87fbeb8204fbebab16ea7a556b9323130ae4f18
CS392-2025-Fall-JJin10 <jjin10@bu.edu>
2025-10-17 17:45:02 -0400

    Update Quiz01_03.java

--- quizzes/01/Code/Quiz01_03.java
+++ quizzes/01/Code/Quiz01_03.java
@@ -18,5 +18,77 @@ public class Quiz01_03 {
 	// HX: No loops (either while-loop or for-loop) is allowed.
 	// HX: Yes, you can use functions (but not recursive functions)
 	// HX: Please do not try to write a HUGH if-then-else mumble jumble!
+		
+		T[] arr = new T[10];
+		arr[0] = x0;
+		arr[1] = x1;
+		arr[2] = x2;
+		arr[3] = x3;
+		arr[4] = x4;
+		arr[5] = x5;
+		arr[6] = x6;
+		arr[7] = x7;
+		arr[8] = x8;
+		arr[9] = x9;
+
+		java.util.function.Consumer<int[]> compareAndSwap = indices -> {
+            if (arr[indices[0]].compareTo(arr[indices[1]]) > 0) {
+                T temp = arr[indices[0]];
+                arr[indices[0]] = arr[indices[1]];
+                arr[indices[1]] = temp;
+            }
+        };
+		compareAndSwap.accept(new int[]{0, 1});
+        compareAndSwap.accept(new int[]{2, 3});
+        compareAndSwap.accept(new int[]{4, 5});
+        compareAndSwap.accept(new int[]{6, 7});
+        compareAndSwap.accept(new int[]{8, 9});
+        
+        compareAndSwap.accept(new int[]{0, 2});
+        compareAndSwap.accept(new int[]{1, 3});
+        compareAndSwap.accept(new int[]{4, 6});
+        compareAndSwap.accept(new int[]{5, 7});
+        
+        compareAndSwap.accept(new int[]{0, 4});
+        compareAndSwap.accept(new int[]{1, 5});
+        compareAndSwap.accept(new int[]{2, 6});
+        compareAndSwap.accept(new int[]{3, 7});
+        
+        compareAndSwap.accept(new int[]{1, 2});
+        compareAndSwap.accept(new int[]{5, 6});
+        compareAndSwap.accept(new int[]{0, 8});
+        compareAndSwap.accept(new int[]{1, 4});
+        compareAndSwap.accept(new int[]{2, 3});
+        compareAndSwap.accept(new int[]{5, 9});
+        compareAndSwap.accept(new int[]{7, 8});
+        
+        compareAndSwap.accept(new int[]{1, 2});
+        compareAndSwap.accept(new int[]{4, 5});
+        compareAndSwap.accept(new int[]{7, 8});
+        compareAndSwap.accept(new int[]{6, 8});
+        
+        compareAndSwap.accept(new int[]{3, 4});
+        compareAndSwap.accept(new int[]{5, 6});
+        compareAndSwap.accept(new int[]{8, 9});
+        
+        compareAndSwap.accept(new int[]{3, 5});
+        compareAndSwap.accept(new int[]{4, 6});
+        compareAndSwap.accept(new int[]{6, 7});
+        
+        compareAndSwap.accept(new int[]{3, 4});
+        compareAndSwap.accept(new int[]{5, 6});
+        
+        return arr;
     }
+
+	public static void main(String[] args){
+		Integer[] result1 = sort10WithNoRecursion(5,2,8,1,9,3,7,4,6,0);
+		System.out.print("Test 2: ");
+		for(String s : result2) {
+			System.out.print(s + " ");
+			
+		}
+		System.out.println();
+		return ;
+	}
 }
