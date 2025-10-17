c0d426fbd999fb6c2469aa64e646679e54fa4ddb
CS392-2025-Fall-JJin10 <jjin10@bu.edu>
2025-10-17 16:26:21 -0400

    Update Quiz01_02.java

--- quizzes/01/Code/Quiz01_02.java
+++ quizzes/01/Code/Quiz01_02.java
@@ -18,11 +18,40 @@ public class Quiz01_02 {
 	// which is of the type Consumer<Character>,
 	// ffcs.accept(fcs) and cs.foritm(fcs) should behave
 	// the same.
-	return new FnList<Character>();
+		FnList<Character> charList = new FnList<>();
+		Consumer<Character> addToList = ch -> charList.append(ch);
+		ffcs.accept(addToList);
+		
+	
+		
+		return charList;
     }
     public static void main (String[] args) {
 	// HX-2025-10-12:
 	// Please write minimal testing code for thirdOrderFun.
-	return /*void*/;
+		Consumer<Consumer<Character>> consumer1 = fcs -> {
+			fcs.accept('J');
+			fcs.accept('A');
+			fcs.accept('V');
+			fcs.accept('A');
+			
+		};
+		FnList<Character> result1 = thirdOrderFun(consumer1);
+		System.out.println("Test 1: " + result1);
+		
+		Consumer<Consumer<Character>> consumer2 = fcs -> {
+			
+		};
+		
+		FnList<Character> result2 = thirdOrderFun(consumer2);
+		System.out.println("Test 2: " + result 2);
+		
+		Consumer<Consumer<Character>> consumer3 = fcs ->{
+			fcs.accept('A');
+			
+		};
+		FnList<Character> result3 = thirdOrderFun(consumer3);
+		system.out.println("Test 3: " + result 3
+		return /*void*/;
     }
 }
